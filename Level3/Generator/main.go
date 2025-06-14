2ac2c5b6ca26b482c7b9fd4ad1f23a3c97d0d26a
Abdul Samad Kirmani <abdul.samadkirmani.samad63@gmail.com>
2025-06-14 11:00:11 +0530

    Generator & Pipelines

---
+++ Level3/Generator/main.go
@@ -0,0 +1,8 @@
+// Create a generator that emits numbers from 1 to 10 with 100ms delay.
+// Consume and print them from the main function.
+
+package main
+
+func main() {
+
+}
---
+++ Level3/Pipeline/main.go
@@ -0,0 +1,11 @@
+// Stage 1: emits numbers 1-20
+// Stage 2: filters even numbers
+// Stage 3: squares them
+// Stage 4: prints the result
+// Use separate goroutines + channels for each stage.
+
+package main
+
+func main() {
+
+}
