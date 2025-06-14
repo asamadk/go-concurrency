10ef75767246dcf717853c40d7568bbe03206c29
Abdul Samad Kirmani <abdul.samadkirmani.samad63@gmail.com>
2025-06-14 11:24:17 +0530

    Pipeline

--- Level3/Pipeline/main.go
+++ Level3/Pipeline/main.go
@@ -6,6 +6,44 @@
 
 package main
 
-func main() {
+import (
+	"time"
+)
 
+func generator(ch chan<- int) {
+	defer close(ch) // Close the channel when done
+	for i := 1; i <= 20; i++ {
+		ch <- i
+		time.Sleep(500 * time.Millisecond)
+	}
+}
+
+func filterEven(ch chan int, evenNum chan<- int) {
+	defer close(evenNum) // Close the evenNum channel when done
+	for num := range ch {
+		if num%2 == 0 {
+			evenNum <- num
+		}
+	}
+}
+
+func square(evenNum chan int, squared chan<- int) {
+	defer close(squared) // Close the squared channel when done
+	for eve := range evenNum {
+		squared <- eve * eve
+	}
+}
+
+func main() {
+	ch := make(chan int)
+	evenNum := make(chan int)
+	squared := make(chan int)
+
+	go generator(ch)
+	go filterEven(ch, evenNum)
+	go square(evenNum, squared)
+
+	for sq := range squared {
+		println("Squared:", sq)
+	}
 }
