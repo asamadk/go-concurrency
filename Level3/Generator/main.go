8cdbba7ce915909ae3b3d92f854869a9f1d4543a
Abdul Samad Kirmani <abdul.samadkirmani.samad63@gmail.com>
2025-06-14 11:06:12 +0530

    Generator

--- Level3/Generator/main.go
+++ Level3/Generator/main.go
@@ -3,6 +3,29 @@
 
 package main
 
-func main() {
+import (
+	"fmt"
+	"time"
+)
 
+// Using unidirectional channels to create a generator that emits numbers
+// Generator can only send values to the channel
+// Consumer can only receive values from the channel
+// This pattern is useful for separating concerns and making the code cleaner.
+func generator(ch chan<- int) {
+	for i := 1; i <= 10; i++ {
+		ch <- i
+		time.Sleep(100 * time.Millisecond)
+	}
+	close(ch)
+}
+
+func main() {
+	ch := make(chan int)
+
+	go generator(ch)
+
+	for c := range ch {
+		fmt.Println("Received:", c)
+	}
 }
